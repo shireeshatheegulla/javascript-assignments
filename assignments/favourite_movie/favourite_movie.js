a3574d94a3b0b03abaf5edb5026c05e956fc0418
shireeshatheegulla <shireesha.theegulla55@gmail.com>
2021-11-25 19:29:56 +0530

    commit on favourite_movies

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,15 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
+    if(operation==="add"){
+        movies.push(movies);
+    }
+    else if(operation==="remove"){
+        movies.pop(movies);
+    }
+    return movies
+    
+
 }
 
 module.exports = favouriteMovie;

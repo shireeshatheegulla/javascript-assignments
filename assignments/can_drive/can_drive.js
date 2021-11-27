d3fcce688823b0db6a44914a7259ec4d490918dc
shireeshatheegulla <shireesha.theegulla55@gmail.com>
2021-11-27 17:00:36 +0530

    commit on can_drvie

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,15 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if(hasDrivingLiscence == false){
+        return "You cannot drive";
+    }else{
+        if((isTired == false) && (isSober == true)){
+            return "You can drive";
+        }
+        if((isTired == true) || (isSober == false)){
+            return "You shouldn't drive";
+        }
+    }
 }
 
 module.exports = CanDrive;

3b9d17b28b1162ba1401f710805b241956b3be17
shireeshatheegulla <shireesha.theegulla55@gmail.com>
2021-11-28 16:55:04 +0530

    commit on runcallbck

--- assignments/run_callback/run_callback.js
+++ assignments/run_callback/run_callback.js
@@ -1,5 +1,8 @@
 function RunCallback(a, b, cb) {
-    // Write you code here, you need to add a and b, pass the result into callback function cb as argument return the result
+    a+b
+    return cb(a+b)
+// Write you code here, you need to add a and b, pass the result into callback function cb as argument
+// return the result
 }
 
 module.exports = RunCallback;

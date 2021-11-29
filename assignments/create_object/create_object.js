6d9e07cc57ddfc387f5dbd4664ef527cb26ef13b
shireeshatheegulla <shireesha.theegulla55@gmail.com>
2021-11-29 19:14:03 +0530

    commit on create-object

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,10 @@
 function CreateObject(arr) {
     // Write your code here
+    const obj={};
+    for(var i=1;i<arr.length;i=i+2){
+        obj[arr[i-1]]=arr[i];
+    }
+    return obj
 }
 
 module.exports = CreateObject;

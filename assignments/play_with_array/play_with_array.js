5843fc7105c3361bed3180e7947867b72e508502
shireeshatheegulla <shireesha.theegulla55@gmail.com>
2021-11-30 15:37:29 +0530

    commit on palywitharrays

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,7 +7,13 @@ function getEven(arr) {
 
         Write you code below
     */
-
+    let arr1=[]
+    for(var i=0;i<arr.length;i++){
+        if(arr[i]%2===0){
+            arr1.push(arr[i])
+        }   
+    }
+    return arr1
 }
 
 function multiplyByN(arr, n) {
@@ -18,6 +24,12 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+    const arr2=[]
+    for(var i=0;i<arr.length;i++){
+        res=arr[i]*n;
+        arr2.push(res);
+    } 
+    return arr2; 
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +40,9 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+    arr.splice(n,1);
+    
+    return arr;
 }
 
 module.exports = {
